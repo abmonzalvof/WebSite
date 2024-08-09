82518ad3f0954048be0f98fe29d4dbd1ab86d7a4
América Monsalvo <america.monzalvo@excent.capital>
2024-08-09 00:48:00 -0600

    (ADD) Add structure and style to template in app component

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,23 @@ module.exports = {
     },
     extend: {
       colors:{
-        'mintcream': '#F3FFF9',
-        'sapphire': '#2950B2',
-        'periwinkle': '#D8C3FF',
-        'richblack': '#01071B',
-        'raisinblack': '#1C1A1F'
-      },
-    },
+        'mintcream': {
+          500: '#F3FFF9'
+        },
+        'sapphire': {
+          225: 'rgb(41,80,178,0.45)',
+          500: '#2950B2'
+        },
+        'periwinkle': {
+          500: '#D8C3FF'
+        },
+        'richblack': {
+          500: '#01071B'
+        },
+        'raisinblack': {
+          500: '#1C1A1F'
+        }
+      },},
   },
   plugins: [],
 }
