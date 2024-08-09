a0bd5da14dfdfd44e97cd76174d785c74498b947
América Monsalvo <america.monzalvo@excent.capital>
2024-08-09 01:07:51 -0600

    (ADD) Add structure, style and data to show navbar

--- src/shared/navbar/navbar.component.ts
+++ src/shared/navbar/navbar.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  public routes: Array<{name: string, link: string}> = [
+    {
+      name: 'Home',
+      link: '/'
+    },
+    {
+      name: 'About me',
+      link: '/about'
+    },
+    {
+      name: 'Certifications',
+      link: '/cert'
+    },
+    {
+      name: 'Experience',
+      link: '/exp'
+    },
+    {
+      name: 'Contact',
+      link: '/contact'
+    }
+  ]
 }
