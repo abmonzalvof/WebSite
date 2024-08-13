cbe557ded142a30c9a6b8a73a7c3763025d67219
abmonzalvof <amonzalvo.laboral@outlook.com>
2024-08-13 01:44:39 -0600

    (ADD) Add all cards in knowledge section

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BandTitle1Component } from '../../shared/band-title-1/band-title-1.component';
 import { CardModel1Component } from '../../shared/card-model-1/card-model-1.component';
 import { CardModel2Component } from '../../shared/card-model-2/card-model-2.component';
+import { Card } from '../../Models/card.model';
 
 @Component({
   selector: 'app-about-me',
@@ -11,5 +12,12 @@ import { CardModel2Component } from '../../shared/card-model-2/card-model-2.comp
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
-
+  public knowledge: Array<Card> = [
+    new Card('Languages',['Typescript', 'Javascript', 'C#', 'Python']),
+    new Card('Platforms',['Angular', '.NET']),
+    new Card('Web', ['HTML', 'CSS', 'SCSS', 'Tailwind', 'Bootstrap']),
+    new Card('Libraries', ['Stripe', 'Angular Material', 'Shepherd (guided tour)', 'RxJs', 'NgRx']),
+    new Card('Applications',['Monday', 'Asana', 'Jira', 'Trello', 'Microsoft office', 'Gsuite', 'NPM']),
+    new Card('Languages',['English: Intermidate / Technical'])
+  ]
 }
