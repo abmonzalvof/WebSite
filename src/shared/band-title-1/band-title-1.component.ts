eb12f179d3e36b1fdfc5a08e2fa0468bc02bc9bf
abmonzalvof <amonzalvo.laboral@outlook.com>
2024-08-12 21:20:02 -0600

    (ADD) Set structure and style in band-title-1

--- src/shared/band-title-1/band-title-1.component.ts
+++ src/shared/band-title-1/band-title-1.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-band-title-1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './band-title-1.component.html',
   styleUrl: './band-title-1.component.scss'
 })
 export class BandTitle1Component {
-
+  @Input() title: 'title' | string = 'title'
 }
