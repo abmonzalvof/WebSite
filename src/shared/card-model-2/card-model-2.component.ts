95cb231accc06ec35926e1d593714f9ba81e3a03
abmonzalvof <amonzalvo.laboral@outlook.com>
2024-08-12 22:31:50 -0600

    (ADD) Add structure and style of  card model 2

--- src/shared/card-model-2/card-model-2.component.ts
+++ src/shared/card-model-2/card-model-2.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-card-model-2',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './card-model-2.component.html',
   styleUrl: './card-model-2.component.scss'
 })
 export class CardModel2Component {
+  @Input() title: string = 'title'
+  @Input() description: string = 'desc'
+  @Input() dates: string = 'dates'
+  @Input() institution: string = 'institution'
+  @Input() link: string = ''
 
 }
