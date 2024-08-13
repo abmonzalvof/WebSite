fe7003c583e6a80620c03ee3f1bcfb603ecb76a8
abmonzalvof <amonzalvo.laboral@outlook.com>
2024-08-12 23:01:27 -0600

    (ADD) Add file to save consts of paths

---
+++ src/Models/paths.ts
@@ -0,0 +1,22 @@
+export const Paths: Array<{name: string, link: string}> = [
+    {
+      name: 'home',
+      link: '/home'
+    },
+    {
+      name: 'about me',
+      link: '/about'
+    },
+    {
+      name: 'certifications',
+      link: '/cert'
+    },
+    {
+      name: 'experience',
+      link: '/exp'
+    },
+    {
+      name: 'contact',
+      link: '/contact'
+    }
+  ]
