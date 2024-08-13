36bda60dde20d5588ff2c0bba4f71a6b689772e2
abmonzalvof <amonzalvo.laboral@outlook.com>
2024-08-12 23:14:56 -0600

    (ADD) Add titlecase pipe to navbar's items

--- src/shared/navbar/navbar.component.ts
+++ src/shared/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Paths } from '../../Models/paths';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
