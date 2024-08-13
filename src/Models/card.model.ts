721bbcc18b121dda6be43ea4b4dd2f54ae487765
abmonzalvof <amonzalvo.laboral@outlook.com>
2024-08-13 01:38:14 -0600

    (ADD) Add function to create two columns in description area

--- src/Models/card.model.ts
+++ src/Models/card.model.ts
@@ -1,10 +1,14 @@
 export class Card {
     public title: string = ''
     public description: string = ''
+    public description2: string = ''
     public items: Array<string> = []
     constructor(title?: string, items?: Array<string>){
         this.title = title ?? ''
         this.items = items ?? []
-        this.description = this.items.join('\n')
+        let aux = this.items.slice(0,5)
+        this.description = aux.join('\n')
+        aux = this.items.slice(5)
+        this.description2 = aux.join('\n')
     }
 }
