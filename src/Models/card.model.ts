e43aace6c648370ee5c0436717af1d96a85c4372
abmonzalvof <amonzalvo.laboral@outlook.com>
2024-08-13 01:21:18 -0600

    (MOD) Changes how to build description parameter

--- src/Models/card.model.ts
+++ src/Models/card.model.ts
@@ -1,8 +1,10 @@
 export class Card {
     public title: string = ''
     public description: string = ''
-    constructor(title?: string, desc?: string){
+    public items: Array<string> = []
+    constructor(title?: string, items?: Array<string>){
         this.title = title ?? ''
-        this.description = desc ?? ''
+        this.items = items ?? []
+        this.description = this.items.join('\n')
     }
 }
